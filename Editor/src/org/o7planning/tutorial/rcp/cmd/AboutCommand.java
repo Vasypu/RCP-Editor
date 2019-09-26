a7bb6f989f1f59fea69e1064d9604dc9b69097a2
Aleksei <lesch.kostenko@yandex.ru>
2019-09-27 01:04:09 +0300

    Added new button (SaveFile), but it does'n work as it need!

--- Editor/src/org/o7planning/tutorial/rcp/cmd/AboutCommand.java
+++ Editor/src/org/o7planning/tutorial/rcp/cmd/AboutCommand.java
@@ -5,20 +5,40 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.internal.dialogs.AboutDialog;
 
 public class AboutCommand extends AbstractHandler implements IHandler {
+	
+		
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		// TODO Auto-generated method stub
-		/*Shell shell = HandlerUtil.getActiveWorkbenchWindow(event).getShell();
-	      AboutDialog dialog = new AboutDialog(shell, SWT.DIALOG_TRIM
-	              | SWT.APPLICATION_MODAL);
-	      dialog.open();*/
-		System.out.print("dsf");
+		
+		Display display = new Display ();
+	    Shell shell = new Shell (display);		    
+		
+		FileDialog dlg = new FileDialog(shell, SWT.OPEN);
+	    String fname = dlg.open();
+	     
+	    if(fname != null) {
+	 	   //textfile.setText(fname);		    		
+	 	}	
+		
+		shell.pack ();
+	    shell.open ();
+
+	    while (!shell.isDisposed ()) {
+	        if (!display.readAndDispatch ()) 
+	            display.sleep ();
+	    }
+	    display.dispose ();
+		System.out.print("Кнопка Open работает");
+	    
 		return null;
 	}
 
