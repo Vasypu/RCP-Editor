10cc46b17d8a121c9f81c3e6bee3d6e938c948e0
root <lesch.kostenko@yandex.ru>
2019-10-07 12:48:59 +0300

    теперь в консоль выводится содержимое текстового файла, не доработано вывдение содержимого на вид приложения

--- Editor/src/editor/MainView.java
+++ Editor/src/editor/MainView.java
@@ -4,6 +4,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
+import org.o7planning.tutorial.rcp.cmd.AboutCommand;
 
 public class MainView extends ViewPart {
 	
@@ -18,12 +19,19 @@ public class MainView extends ViewPart {
 		// TODO Auto-generated method stub
 		Text text = new Text(parent, SWT.BORDER);
         text.setText("Imagine a fantastic user interface here");
+        AboutCommand command = new AboutCommand();
+        
 	}
 
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public static void getText(String s) {
+		Text text = new Text (null, SWT.BORDER);
+		text.setText(s);
 	}
 
 }
--- Editor/src/org/o7planning/tutorial/rcp/cmd/AboutCommand.java
+++ Editor/src/org/o7planning/tutorial/rcp/cmd/AboutCommand.java
@@ -1,5 +1,9 @@
 package org.o7planning.tutorial.rcp.cmd;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -11,6 +15,8 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.internal.dialogs.AboutDialog;
+
+import editor.MainView;
 
 public class AboutCommand extends AbstractHandler implements IHandler {
 	
@@ -28,15 +34,36 @@ public class AboutCommand extends AbstractHandler implements IHandler {
 		long handle = 0;
 		FileDialog dlg = new FileDialog(Shell.gtk_new(Display.getDefault(), handle), SWT.OPEN);
 	    String fname = dlg.open();
-	    System.out.println(HandlerUtil.getActiveShell(event));
+	    //System.out.println(HandlerUtil.getActiveShell(event));
 	    
 	    Text text = new Text(dlg.getParent(), SWT.NONE);
 	    text.setText(dlg.getParent().getText().toString());
-	    System.out.print(dlg.getText());
-	     
+	    System.out.print(dlg.getParent().getText().toString());
+	    
 	    if(fname != null) {
-	 	   System.out.println();		    		
+	 	   System.out.println(dlg.getText());		    		
 	 	}
+	    try {
+			FileReader file = new FileReader(fname);
+			char [] buffer = new char[55];
+			
+			 int i=-1;
+	            while((i=file.read())!=-1){
+	              
+	                //System.out.print((char)i);
+	                MainView.getText((char)i); 
+	            } 
+	            /*int readed;
+				while((readed = file.read(buffer)) != -1) {
+										
+	} */
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		/*shell.pack ();
 	    shell.open ();*/
@@ -46,7 +73,7 @@ public class AboutCommand extends AbstractHandler implements IHandler {
 	            display.sleep ();
 	    }
 	    display.dispose ();*/
-		System.out.println("Кнопка Open работает");
+		//System.out.println("Кнопка Open работает");
 	    
 		return null;
 	}
