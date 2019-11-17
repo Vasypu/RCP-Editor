20e5e4b5c47ee9c18efb4db398142b67b9d862b0
alexey.kostenko <lesch.kostenko@yandex.ru>
2019-11-18 00:33:16 +0300

    Добавил поддержку нескольких документов в блокноте

--- Editor/src/editor/OpenFileCmdHandler.java
+++ Editor/src/editor/OpenFileCmdHandler.java
@@ -6,20 +6,22 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 public class OpenFileCmdHandler extends AbstractHandler implements IHandler {
 
+	private int instanceNum = 0;	
+
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		HandlerUtil.getActiveShell(event);
+		HandlerUtil.getActiveShell(event);		
 		TextView v;
-		try {
-			v = (TextView) HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().showView(TextView.ID);
+		try {			
+			v = (TextView) HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().showView(TextView.ID, Integer.toString(instanceNum++), IWorkbenchPage.VIEW_ACTIVATE);
 			v.readFile();
-		} catch (PartInitException e) {
-			// Shell shell = new Shell();
+		} catch (PartInitException e) {			
 			int style = SWT.ERROR | SWT.OK | SWT.CANCEL;
 			MessageBox messageBox = new MessageBox(HandlerUtil.getActiveShell(event), style);
 			messageBox.setText("Ошибка");
